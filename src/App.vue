e1163f008965567c1b11d93ea290f640a80097c8
Lisongzhi7 <1378271659@qq.com>
2020-08-28 22:45:42 +0800

    首页基本完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,245 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+      <div class="fd-head">
+        <div class="fd-head-center">
+          <div class="fd-logo-box">
+            <span>博客网站</span>
+          </div>
+          <div class="fd-logo-tile">
+            <span>博客首页</span>
+            <span>文章分类</span>
+            <span>文章归档</span>
+            <span>友情链接</span>
+            <span>关于我</span>
+          </div>
+        </div>
+      </div>
+      <div class="fd-center">
+      <div class="fd-article-card">
+        <div class="fd-card" v-for="(item, index) of articles">
+          <div class="fd-image">{{item.title}}</div>
+          <div class="fd-desc">{{item.desc}}</div>
+          <div class="fd-time">
+            <span><i class="el-icon-share"></i>{{item.time}}</span>
+            <span> <i class="el-icon-s-custom"></i>{{item.author}}</span>
+          </div>
+          <div class="fd-tag">
+            <span v-for="(tg, index) of item.tag">{{tg.name}}</span>
+          </div>
+
+        </div>
+
+      </div>
+
+      </div>
+      <div class="fd-bottom">
+        蜀ICP备18020377号
+      </div>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+        articles: [
+          {
+            title: "vue学习笔记",
+            desc: "2020年6月的一些业务人文读书记录，记录一下常识gsdggsgdfgfd:",
+            time: "2020年06月23日",
+            author: "李松直",
+            tag: [
+              {
+                name: "Vue"
+              },
+              {
+                name: "javaScript"
+              },
+              {
+                name: "前端"
+              },
+              {
+                name: "学习笔记"
+              }
+            ]
+          },
+          {
+            title: "Html学习笔记",
+            desc: "2020年6月的一些业务人文读书记录，记录一下常识gsdggsgdfgfd:",
+            time: "2020年06月24日",
+            author: "李松直",
+            tag: [
+              {
+                name: "Vue"
+              },
+              {
+                name: " 初级"
+              },
+              {
+                name: "Html"
+              },
+              {
+                name: "学习笔记"
+              }
+            ]
+          },
+          {
+            title: "Git学习笔记",
+            desc: "2020年6月的一些业务人文读书记录，记录一下常识gsdggsgdfgfd:",
+            time: "2020年06月26日",
+            author: "李松直",
+            tag: [
+              {
+                name: "Git"
+              },
+              {
+                name: "代码管理"
+              }
+            ]
+          },
+          {
+            title: "日常读书",
+            desc: "2020年6月的一些业务人文读书记录，记录一下常识gsdggsgdfgfd:",
+            time: "2020年06月36日",
+            author: "罗康元",
+            tag: [
+              {
+                name: "读书"
+              }
+            ]
+          }
+        ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*, html, body{
+  margin: 0;
+  padding: 0;
+}
+#app{
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+}
+.fd-head{
+  width: 100%;
+  height: 80px;
+  background: #fff;
+  box-shadow: 0 15px 35px rgba(50, 50, 93, .1), 0 5px 15px rgba(0, 0, 0, .07);
+  display: flex;
+  justify-content: center;
+}
+.fd-head-center{
+  width: 1400px;
+  height: 100%;
+  display: flex;
+}
+.fd-logo-box{
+  width: 400px;
+  height: 100%;
+}
+.fd-logo-box span{
+  font-size: 20px;
+  height: 100%;
+  font-weight: bold;
+  color: #000;
+  display: flex;
+  align-items: center;
+}
+.fd-logo-tile{
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+.fd-logo-tile span{
+  margin-left: 10px;
+  font-size: 20px;
+}
+.fd-center{
+  display: flex;
+  flex: 1;
+  justify-content: center;
+}
+.fd-bottom{
+  display: flex;
+  width: 100%;
+  height: 60px;
+  background: black;
+  color: white;
+  justify-content: center;
+  align-items: center;
+}
+.fd-article-card{
+  width: 1400px;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+}
+.fd-card{
+  width: 400px;
+  height: 380px;
+  box-shadow: 0 15px 35px rgba(50, 50, 93, .1), 0 5px 15px rgba(0, 0, 0, .07);
+  margin-top: 30px;
+  border-radius: 10px;
+  margin-left: 30px;
+  margin-bottom: 10px;
+
+}
+.fd-image{
+  width: 100%;
+  height: 200px;
+  background: url("./assets/image/card1.jpg") ;
+  background-size: cover;
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+ border-top-left-radius: 10px;
+ border-top-right-radius: 10px;
+}
+.fd-desc{
+  font-size: 16px;
+  margin: 10px 10px 20px 10px;
+  line-height: 27px
+}
+.fd-time{
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #9a9696;
+ padding-bottom: 15px;
+}
+.fd-time span:nth-child(1){
+  margin-left: 10px;
+}
+.fd-time span:nth-child(2){
+  margin-right: 10px;
+}
+.fd-tag{
+  height: 65px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.fd-tag span{
+  background: coral;
+  margin-left: 5px;
+  margin-right: 5px;
+  padding: 5px 15px;
+  border-radius: 10px;
+  color: #fff;
+
+
 }
 </style>
